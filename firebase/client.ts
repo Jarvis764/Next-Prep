93360871f2e34a82d63defde092b322d3e6294c2
Shreesha <shreesha.alevoor664@gmail.com>
2025-12-06 02:09:50 +0530

    Client.ts 2.0

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import {getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdckSjKeG3i6_gw-CMK8ijFvYxeCNoWr4",
-  authDomain: "nextprep-be126.firebaseapp.com",
-  projectId: "nextprep-be126",
-  storageBucket: "nextprep-be126.firebasestorage.app",
-  messagingSenderId: "1014180916980",
-  appId: "1:1014180916980:web:d7b584f4b1dff90d12f8a1",
-  measurementId: "G-CQEKHBH44F"
+  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
+   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
